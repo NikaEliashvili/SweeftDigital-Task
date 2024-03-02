0d10bf358002536ad27b38334a3077509378e435
NikaEliashvili <nikaeliashvili18@outlook.com>
2024-03-03 02:34:11 +0400

    fixed historyPage component

--- src/pages/historyPage/HistoryPage.tsx
+++ src/pages/historyPage/HistoryPage.tsx
@@ -1,30 +1,34 @@
-import { useState } from "react";
 import HistoryTerm from "../../components/HistoryTerm/HistoryTerm";
 import "./historyPage.css";
-import { Image } from "../../types/types";
 import { TiArrowBack } from "react-icons/ti";
 import { Link } from "react-router-dom";
 
 export default function HistoryPage() {
-  const [history, setHistory] = useState<{
-    [query: string]: Image[];
-  }>(JSON.parse(localStorage.getItem("history") as string) || {});
+  // const [history, setHistory] = useState<{
+  //   [query: string]: Image[];
+  // }>(JSON.parse(localStorage.getItem("history") as string) || {});
 
+  const history =
+    JSON.parse(localStorage.getItem("history") as string) || {};
   return (
     <div className="history-page">
-      <h1 className="title">History Page</h1>
+      <h1 className="title">Search History</h1>
       <Link to="/" className="back-home-btn">
         <TiArrowBack />
         <span>Home</span>
       </Link>
       <div className="all-terms">
-        {Object.keys(history).map((query, i) => (
-          <HistoryTerm
-            key={i + 1}
-            searchedQuery={query}
-            searchedImages={history[query]}
-          />
-        ))}
+        {Object.keys(history).length > 0 ? (
+          Object.keys(history).map((query, i) => (
+            <HistoryTerm
+              key={i + 1}
+              searchedQuery={query}
+              searchedImages={history[query]}
+            />
+          ))
+        ) : (
+          <p className="no-history">No history found... </p>
+        )}
       </div>
     </div>
   );
